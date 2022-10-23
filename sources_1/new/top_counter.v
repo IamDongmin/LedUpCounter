ea61cd8c4ca9d379732cf09068318d016b463a2b
unknown <IamDongmin>
2022-10-23 22:05:49 +0900

    다섯번째 프로젝트

---
+++ sources_1/new/top_counter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+
+module top_counter(
+    input i_clk,
+    input i_reset,
+
+    output [7:0] o_led
+
+    );
+
+    wire w_clk;
+
+    clock_divider prescaler(
+        .i_clk(i_clk),
+        .i_reset(i_reset),
+        .o_clk(w_clk)
+    );
+
+    counter counter_8bit(
+        .i_clk(w_clk),
+        .i_reset(i_reset),
+        .o_counter(o_led)
+    );
+
+endmodule
